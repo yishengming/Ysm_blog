b96cdaec5b6b1fed5e82ed92a755362e7bf1be5b
yishengming <82427835+yishengming@users.noreply.github.com>
2023-06-17 18:38:38 +0800

    Delete api directory

--- api/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-$img_array = glob('img/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
-if(count($img_array) == 0) die('没有找到图片文件。MuaOoO ~ '.dirname(__FILE__).'api/img/');
-header('Content-Type: image/webp');
-echo(file_get_contents($img_array[array_rand($img_array)]));
